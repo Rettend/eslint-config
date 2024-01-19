799a357d4c3695f627bda22deccc22c7ba20ff57
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2024-01-19 12:25:16 +0100

    build: add dist to donwload fork

---
+++ dist/chunk-BHAFELBA.js
@@ -0,0 +1,27 @@
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+}) : x)(function(x) {
+  if (typeof require !== "undefined")
+    return require.apply(this, arguments);
+  throw Error('Dynamic require of "' + x + '" is not supported');
+});
+var __esm = (fn, res) => function __init() {
+  return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
+};
+var __commonJS = (cb, mod) => function __require2() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+
+// node_modules/.pnpm/tsup@8.0.1_postcss@8.4.32_typescript@5.3.3/node_modules/tsup/assets/esm_shims.js
+var init_esm_shims = __esm({
+  "node_modules/.pnpm/tsup@8.0.1_postcss@8.4.32_typescript@5.3.3/node_modules/tsup/assets/esm_shims.js"() {
+    "use strict";
+  }
+});
+
+export {
+  __require,
+  __commonJS,
+  init_esm_shims
+};
